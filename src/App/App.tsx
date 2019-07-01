63c74747d030836206556005da865ec619c50252
Mario Galeno <mgaleno@solstice.com>
2019-07-01 10:16:47 -0500

    Setup app scaffolding plus router-dom and scss packages

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,28 +1,63 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 
-import logo from '../logo.svg';
-import './App.css';
+import './App.scss';
+
+const Header = () => (
+  <header>
+    <Navbar />
+  </header>
+)
+
+const Navbar = () => (
+  <>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/blog">Blog</Link></li>
+      </ul>
+    </nav>
+    <span className="title">
+      <p>MARIO GALENO</p>
+    </span>
+  </>
+);
+
+const Content = () => (
+  <main>
+    <Switch>
+      <Route path="/" exact component={() => (<h1>Home</h1>)} />
+      <Route path="/about" exact component={() => (
+        <div>
+          <h1>dd</h1>
+          <h1>about</h1>
+          <h1>about</h1>
+          <h1>about</h1>
+          <h1>about</h1>
+          <h1>about</h1>
+        </div>
+      )} />
+      <Route path="/blog" exact component={() => (<h1>blog</h1>)} />
+    </Switch>
+  </main>
+)
+
+const Footer = () => (
+  <footer>
+    <p>Mario Galeno <span>&copy;2019</span></p>
+  </footer>
+)
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Content />
+      <Footer />
+    </Router>
   );
 }
 
